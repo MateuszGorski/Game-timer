9708a65ca936254b97ae7d0ae8658f2034471c84
Mateusz Górski <mgorski.lbn@gmail.com>
2019-01-18 12:45:35 +0100

    updated timer

--- script/timer.js
+++ script/timer.js
@@ -1,25 +1,22 @@
-//Define vars to hold time values
 let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
-//Define vars to hold "display" value
+
 let displaySeconds = 0;
 let displayMinutes = 0;
 let displayHours = 0;
 
-//Define var to hold setInterval() function
+
 let interval = null;
 
-//Define var to hold stopwatch status
 let status = "stopped";
 
-//Stopwatch function (logic to determine when to increment next value, etc.)
+
 function stopWatch() {
 
     seconds++;
 
-    //Logic to determine when to increment next value
     if (seconds / 60 === 1) {
         seconds = 0;
         minutes++;
@@ -31,7 +28,6 @@ function stopWatch() {
 
     }
 
-    //If seconds/minutes/hours are only one digit, add a leading 0 to the value
     if (seconds < 10) {
         displaySeconds = "0" + seconds.toString();
     } else {
@@ -50,33 +46,31 @@ function stopWatch() {
         displayHours = hours;
     }
 
-    //Display updated time values to user
     document.getElementById("display").innerHTML = displayHours + ":" + displayMinutes + ":" + displaySeconds;
 
 }
-
 
 
 function startStop() {
 
     if (status === "stopped") {
 
-        //Start the stopwatch (by calling the setInterval() function)
+
         interval = window.setInterval(stopWatch, 1000);
-        document.getElementById("startStop").innerHTML = "Stop";
+        document.getElementById("startStop").innerHTML = "";
         status = "started";
 
     } else {
 
         window.clearInterval(interval);
-        document.getElementById("startStop").innerHTML = "Start";
+        document.getElementById("startStop").innerHTML = "";
         status = "stopped";
 
     }
 
 }
 
-//Function to reset the stopwatch
+
 function reset() {
 
     window.clearInterval(interval);
@@ -84,6 +78,6 @@ function reset() {
     minutes = 0;
     hours = 0;
     document.getElementById("display").innerHTML = "00:00:00";
-    document.getElementById("startStop").innerHTML = "Start";
+    document.getElementById("startStop").innerHTML = "";
 
 }
